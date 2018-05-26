0fde23e607cfbdd97bca15fbe4fbb53d36d6143f
rogeryang <ylc8131@qq.com>
2018-05-26 15:36:49 +0800

    init_vim

---
+++ init_vim.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+_home=~/.vim/pack/myplugins/start
+
+mkdir -p $_home
+
+cd $_home
+
+git clone https://github.com/prabirshrestha/vim-lsp.git
+git clone https://github.com/prabirshrestha/async.vim.git
+
+mkdir ~/opt
+cd ~/opt
+git clone https://github.com/cquery-project/cquery --single-branch --depth=1
+cd cquery
+git submodule update --init && ./waf configure build
+
+
